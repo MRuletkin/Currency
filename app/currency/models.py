33b712870d71ee0979b2968a8db5701f0853f97e
MRuletkin <mruletkin6@gmail.com>
2021-10-19 17:21:49 +0300

    Homework_5

--- app/currency/models.py
+++ app/currency/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Rate(models.Model):
+    buy = models.DecimalField(max_digits=6, decimal_places=2)
+    sale = models.DecimalField(max_digits=6, decimal_places=2)
+    created = models.DateTimeField()
+    type = models.CharField(max_length=3)  # noqa: A003
+    source = models.CharField(max_length=25)
+
+
+class ContactUs(models.Model):
+    id = models.AutoField(primary_key=True)
+    email_from = models.EmailField(max_length=254)
+    subject = models.CharField(max_length=50)
+    message = models.CharField(max_length=100)
--- app/currency/views.py
+++ app/currency/views.py
@@ -1,7 +1,26 @@
-from django.shortcuts import render
+from currency.models import Rate, ContactUs
+
 from django.http.response import HttpResponse
 
 
-# Create your views here.
-def hello_world(request):
-    return HttpResponse('Hello_world')
+def rate_list(request):
+
+    results = []
+    rates = Rate.objects.all()
+
+    for rate in rates:
+        results.append(
+            f'ID: {rate.id}, sale: {rate.sale}, buy: {rate.buy}, created: {rate.created}, source: {rate.source}<br>')
+
+    return HttpResponse(str(results))
+
+
+def contact_us(request):
+    results = []
+    obj = ContactUs.objects.all()
+
+    for _ in obj:
+        results.append(
+            f'ID: {_.id}, sale: {_.email_from}, buy: {_.subject}, created: {_.message}<br>')
+
+    return HttpResponse(str(results))
